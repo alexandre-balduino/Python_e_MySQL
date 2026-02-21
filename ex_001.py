bfe00b5b9eec86b4b7f59fd82c91f1566e607397
* <alexandrebalduino17@gmail.com>
2026-02-21 13:27:25 -0300

    criei um arquivo separado para senhas e adicionei ao .gitignore

---
+++ ex_001.py
@@ -0,0 +1,15 @@
+'''
+Faça uma lista com o nome de 
+todas as gafanhotas mulheres
+'''
+
+import mysql.connector 
+
+conexao = mysql.connector.connect(
+    host
+    user
+    password
+    database
+)
+
+
